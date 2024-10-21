6513d75fb4868890dd7df3dd287d4813ec67d4a7
uLeozinxdd <00001089186873sp@al.educacao.sp.gov.br>
2024-10-21 20:57:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Programação',
+    'O que é Phyton ?',
+    'O Phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
+
+criaCartao (
+    'Artes',
+    ' Quem pintou "Guernica"?',
+    'Pablo Picasso'
+)
+
+criaCartao( 
+    'Ed.Fisica',
+    'Qual a altura da rede de vôlei nos jogos masculino e feminino?',
+    '2,43 m e 2,24 m'
+)
+
+criaCartao( 
+    'História',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Paleolítico'
+)
